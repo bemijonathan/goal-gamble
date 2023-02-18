84a7268b23cb8301eda14780ca32edbf64af36be
Jonathan Atiene <34762800+bemijonathan@users.noreply.github.com>
2023-02-18 09:17:37 +0100

    fixed issue with nexus and graphQL

--- packages/backend/lib/domain/user/query.ts
+++ packages/backend/lib/domain/user/query.ts
@@ -1,9 +1,8 @@
-import {queryType} from "nexus";
 import {Hello} from "./types";
 import {queryField} from "@nexus/schema";
 
 export const hello =  queryField('hello', {
-    type: 'Hello',
+    type: Hello,
     resolve(){
         return {name: 'Hello'}
     }
--- packages/backend/lib/schema.ts
+++ packages/backend/lib/schema.ts
@@ -2,25 +2,17 @@
 import { makeSchema } from 'nexus'
 import { nexusPrisma } from 'nexus-plugin-prisma'
 import { join } from 'path'
-// import * as types from './domain'
+import * as types from './domain'
 
 export const schema = makeSchema({
-  types: {
-
-  },
+  types,
   outputs: {
     typegen:join(__dirname, '../generated/nexus-typegen.ts'),
     schema:join(__dirname, '../schema.graphql'),
   },
   plugins: [
-    nexusPrisma(
-    //     {
-    //   experimentalCRUD: true,
-    //   inputs: {
-    //     prismaClient: '',
-    //   }
-    // }
-    )
+    nexusPrisma({
+      experimentalCRUD: true,
+    })
   ],
-  // asdf
 })
