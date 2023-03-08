530882b41a692647477b2bcfb29b009b84f0047e
LizzyKate <onyenekwelizabeth@gmail.com>
2023-03-08 05:01:28 +0100

    auth pages and error pages and dashboard layout

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import "antd/dist/reset.css";
 import { SignUp } from "./pages/AuthLayout/SignUp";
 import { Route, Routes } from "react-router-dom";
+import { SignIn } from "./pages/AuthLayout/SignIn";
+import { Dashboard } from "./pages";
+import { Error } from "./pages/error";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Dashboard />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   );
 }
---
+++ packages/frontend/src/pages/AuthLayout/SignIn.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Row, Col, Button, Checkbox, Form, Input } from "antd";
+
+const onFinish = (values: any) => {
+  console.log("Success:", values);
+};
+
+const onFinishFailed = (errorInfo: any) => {
+  console.log("Failed:", errorInfo);
+};
+
+export const SignIn: React.FC = () => (
+  <Row justify="center" style={{ marginTop: "32px" }}>
+    <Col span={12}>
+      <Form
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+        layout="vertical"
+      >
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[{ required: true, message: "Please input your email!" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{ offset: 8, span: 16 }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </Col>
+  </Row>
+);
